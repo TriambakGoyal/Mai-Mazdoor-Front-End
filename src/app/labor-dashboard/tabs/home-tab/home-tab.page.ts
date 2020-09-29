f2234e5c4513e25be3abc287f531c1335e7bd33d
sreejitkar <sreejit.kar@science.christuniversity.in>
2020-09-29 23:20:00 +0530

    Integarted GETJobs API

--- src/app/labor-dashboard/tabs/home-tab/home-tab.page.ts
+++ src/app/labor-dashboard/tabs/home-tab/home-tab.page.ts
@@ -1,4 +1,23 @@
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { __values } from 'tslib';
+
+export interface IJob {
+  job_id: number;
+  contractor: {
+    contractor_id: number;
+    contractor_name: string;
+    contractor_age: number;
+    contractor_sex: string,
+    contractor_location: string;
+    contractor_rating: string;
+  },
+  job_name: string,
+  job_description: string,
+  job_start_date: string,
+  job_end_date: string
+}
 
 @Component({
   selector: 'app-home-tab',
@@ -7,9 +26,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTabPage implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  job_list: IJob[];
 
   ngOnInit() {
+    this.http.get<IJob[]>("http://127.0.0.1:8000/jobs/get-all-jobs/").subscribe(
+      response => {
+        this.job_list = response;
+        console.log(this.job_list)
+      }
+    );
   }
 
 }
