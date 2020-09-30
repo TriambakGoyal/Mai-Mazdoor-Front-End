537577f77351010b8d548b4db9fd3c9632d3b9e4
sreejitkar <sreejit.kar@science.christuniversity.in>
2020-09-30 09:46:38 +0530

    Added SIgnUP interface

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface ISignup {
+  s_name: string,
+  s_gender: string,
+  s_age: string,
+  s_location: string,
+  s_phno: string,
+}
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
