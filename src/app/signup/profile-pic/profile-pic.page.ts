700746649059cd662a88fa9c5a6d3a735a0eaf02
TriambakGoyal <triambak.goyal@science.christuniversity.in>
2020-10-06 20:33:17 +0530

    Profile picture page complete

--- src/app/signup/profile-pic/profile-pic.page.ts
+++ src/app/signup/profile-pic/profile-pic.page.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-pic.page.scss'],
 })
 export class ProfilePicPage implements OnInit {
-
+imgUrl : string = '/assets/logo/logo_1.png';
+FiletoUpload: File =null;
   constructor() { }
 
   ngOnInit() {
   }
+  handleFileInput(file : FileList){
+    this.FiletoUpload= <File>file.item(0);
 
+    console.log(this.FiletoUpload);
+
+    //Show Image preview
+    var reader = new FileReader();
+    reader.onload = (event:any) => {
+      this.imgUrl= event.target.result;
+    }
+    reader.readAsDataURL(this.FiletoUpload);
+
+  }
 }
